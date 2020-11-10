c3c05283fd505aeaa34a494459f63c561290ba96
stanislav <1991>
2020-11-10 20:38:28 +0300

    дата 19.01.2020

--- test/ru/job4j/info.java
+++ test/ru/job4j/info.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 public class info {
     public static void main(String[] args) {
-        System.out.println("19.01.2020;");
+        System.out.println("19.01.2020:");
     }
 }
