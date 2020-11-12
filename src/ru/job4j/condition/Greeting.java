aa731688cbbf067633cffc83aec67f2b1db2f3b8
stanislav <1991>
2020-11-12 09:27:57 +0300

    Greeting

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idia = "I like Java!";
+        idia +=  "But I am a newbie. ";
+        int year = 2020;
+        idia += year;
+        System.out.println(idia);
+    }
+}
