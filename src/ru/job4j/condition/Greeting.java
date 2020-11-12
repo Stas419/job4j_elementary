88e6cadd072793433a767faa9f8c701d7bd62ce4
stanislav <1991>
2020-11-12 19:33:22 +0300

    Greeting

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,10 +2,15 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idia = "I like Java!";
-        idia +=  "But I am a newbie. ";
-        int year = 2020;
-        idia += year;
-        System.out.println(idia);
+//        String idia = "I like Java!";
+//        idia +=  "But I am a newbie. ";
+//        int year = 2020;
+//        idia += year;
+//        System.out.println(idia);
+
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println( d);gssa
     }
 }
