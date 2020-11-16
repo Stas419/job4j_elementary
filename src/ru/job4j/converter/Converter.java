79a19a828b10b7ebaa98c304dbafcdfa48bbdc3d
stanislav <1991>
2020-11-16 08:08:28 +0300

    Converter

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -13,10 +13,11 @@ public class Converter {
         int in = 140;
         int in2 = 250;
         int expected = 2;
+        int expected2 = 2;
         int out = Converter.rubleToEuro(in);
         int out2 = Converter.rubleToDollar(in2);
         boolean passed = out == expected;
-        boolean passed2 = out2 == expected;
+        boolean passed2 = out2 == expected2;
         System.out.println("140 rubles are " + passed + " euro");
         System.out.println("140 rubles are " + passed2 + " dollar");
     }
