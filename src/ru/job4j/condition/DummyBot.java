2a6f0edfa8f03777ab3069b1d022f78cf9d69288
stanislav <1991>
2020-11-18 20:31:40 +0300

    DummyBot

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -5,22 +5,17 @@ public class DummyBot {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
+            System.out.println(rsl);
         }
-        else if ("пока".equals(question)) { /* заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить. */
-            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+        else if ("пока".equals(question)) {
             rsl = "До скорой встречи.";
-        }
-        else{
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
     }
 
 
     public static void main(String[] args) {
-        String rsl = DummyBot.answer("Привет, Бот");
-        System.out.println(rsl);
-        rsl = DummyBot.answer("Пока.");
+        String rsl = DummyBot.answer("Привет, Бот.");
         System.out.println(rsl);
     }
 }
