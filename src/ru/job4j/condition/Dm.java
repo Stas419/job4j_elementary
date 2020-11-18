519fb8a23691a19d1175901899ecdb6054a7924e
stanislav <1991>
2020-11-18 20:43:17 +0300

    DummyBot

---
+++ src/ru/job4j/condition/Dm.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class Dm {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте  другой вопрос!";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+            System.out.println(rsl);
+        }
+        else if ("пока".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+    }
+}
