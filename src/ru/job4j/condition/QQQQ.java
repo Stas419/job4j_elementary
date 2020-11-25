e125c18d81079a7250aa0eeecf067b2a791df63e
stanislav <1991>
2020-11-25 19:06:00 +0300

    Poin

---
+++ src/ru/job4j/condition/QQQQ.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+public class QQQQ {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 5;
+        int c = 1;
+
+        if(a>b){
+            System.out.println("A больше");
+        }
+
+        else if(c > b){
+            System.out.println("C больше");
+        }
+
+        else{
+            System.out.println("B больше");
+        }
+    }
+
+
+
+}
+
+
+
