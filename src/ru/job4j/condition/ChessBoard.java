8f15b1eb40be505928e19e2b8d6a8db95d9b8634
stanislav <1991>
2020-11-27 21:17:49 +0300

    ChessBoard

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -2,15 +2,24 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        int rsl = 0;
-        if(x1 == x2 && y1 == y2){
-            rsl = Math.abs(x1 - x2);
+        int rsl = 1;
+        if ( x2 == y2) {
+            rsl = Math.abs(x2 - x1);
             rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+        }
+        else{
+            rsl = 0;
         }
         return rsl;
     }
 
 
+
+
+    public static void main(String[] args){
+       int s =  ChessBoard.way(1,0,5,5);
+       System.out.print(s);
+    }
 
 
 }
