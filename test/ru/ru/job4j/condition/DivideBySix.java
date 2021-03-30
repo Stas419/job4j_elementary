3c43e4efe45f8f2dd12e8a0e491878920c45c90a
stanislav <1991>
2021-03-30 12:50:46 +0300

    Загрузка файлов на github [#405525]

---
+++ test/ru/ru/job4j/condition/DivideBySix.java
@@ -0,0 +1,28 @@
+package ru.ru.job4j.condition;
+
+public class DivideBySix {
+    public static String checkNumber(int number){
+        String rsl;
+        if (...) {
+            if (...) {
+                rsl = ...;
+            } else {
+                rsl = ...;
+            }
+        } else {
+            if (...) {
+                rsl = ...;
+            } else {
+                rsl = ...;
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] agrs){
+
+        String a = DivideBySix.checkNumber(2);
+
+        System.out.print(a);
+    }
+}
