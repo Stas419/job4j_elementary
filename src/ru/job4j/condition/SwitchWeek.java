a822760b751a999d3eafdf074f0d92c5331eac82
stanislav <1991>
2021-03-30 22:55:11 +0300

    4.6.1. Switch [#405455]

---
+++ src/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+
+    public static String nameOfDay(int day){
+        String name;
+        switch(day) {
+            case 1:
+                name = "понедельник";
+                break;
+            case 2:
+                name = "вторник";
+                break;
+            case 3:
+                name = "среда";
+                break;
+            case 4:
+                name = "четверг";
+                break;
+            case 5:
+                name = "пятница";
+                break;
+            case 6:
+                name = "субота";
+                break;
+            case 7:
+                name = "воскресение";
+                break;
+            default:
+                name = "в неделе 7 дней";
+                break;
+        }
+        return name;
+    }
+    public static void main(String[] args){
+       String name = SwitchWeek.nameOfDay(5);
+       System.out.println(name);
+    }
+
+}
