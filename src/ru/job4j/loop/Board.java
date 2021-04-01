8c424ed7b35ee1312e0af4b9962d45159b8fc42e
stanislav <1991>
2021-04-01 19:50:47 +0300

    5.2. Создать программу, вычисляющую факториал. [#405565]

--- src/ru/job4j/loop/Board.java
+++ src/ru/job4j/loop/Board.java
@@ -5,7 +5,7 @@ public class Board {
         for (int i = 0; i < width; i++ ) {
             for (int j = 0; j < height; j++ ) {
 
-                if (i  % 2 == 0 & j  % 2 == 0  ) {
+                if ((i + j) % 2 == 0  ) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");
