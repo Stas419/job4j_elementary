d129e3dd7de6858ac3ee536aba57927e3ed638c0
stanislav <1991>
2021-04-17 21:37:50 +0300

    6.4. Слово начинается с ... [#405468]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (word[i] != post[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'H', 'e'};
+        System.out.println(EndsWith.endsWith(word, post));
+    }
+}
