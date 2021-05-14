71b0f4b10f142f67d6b8d3801f65f60b21e6c90f
stanislav <1991>
2021-05-14 19:46:29 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587 #143237]

--- src/ru/job4j/condition/Pint.java
+++ src/ru/job4j/condition/Pint.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
-public class Pint {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double vichetanieX = x2 - x1;
-        double vichetanieY = y2 - y1;
-        double rsl = Math.pow(vichetanieX, 2) + Math.pow(vichetanieY, 2);
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-    }
-}
+//public class Pint {
+//    public static double distance(int x1, int y1, int x2, int y2) {
+//        double vichetanieX = x2 - x1;
+//        double vichetanieY = y2 - y1;
+//        double rsl = Math.pow(vichetanieX, 2) + Math.pow(vichetanieY, 2);
+//        return rsl;
+//    }
+//
+//    public static void main(String[] args) {
+//        double result = Point.distance(0, 0, 2, 0);
+//        System.out.println("result (0, 0) to (2, 0) " + result);
+//    }
+//}
